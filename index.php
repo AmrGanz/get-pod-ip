b80d30d51fdb988689dff6795e294c28cd71b516
Amr Elganzory <43642001+AmrGanz@users.noreply.github.com>
2020-08-16 11:05:35 +0400

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
-<div>
 <?php 
  echo "Pod IP: ".$_SERVER['SERVER_ADDR'] ;
  ?>
-</div>
